27b313e925fe42e95a904d6c70e1ae56caef0b41
vhorielova <horielova.viktoria@gmail.com>
2024-03-11 14:53:40 +0200

    renamed to TlButtonView

---
+++ BooksOnShelves/Main/HomeView/BooksView.swift
@@ -0,0 +1,18 @@
+//
+//  BooksView.swift
+//  BooksOnShelves
+//
+//  Created by Віка Горєлова on 04.03.2024.
+//
+
+import SwiftUI
+
+struct BooksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BooksView()
+}
---
+++ BooksOnShelves/Main/HomeView/SortShelfViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SortShelfViewViewModel.swift
+//  BooksOnShelves
+//
+//  Created by Віка Горєлова on 04.03.2024.
+//
+
+import Foundation
